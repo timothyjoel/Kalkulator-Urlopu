8556fba7ba4adce5989594cb0fda6db102038164
Tymoteusz Stokarski <tjoelstokarski@gmail.com>
2020-11-03 14:38:58 +0100

    Initial Commit

---
+++ Kalkulator Urlopu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kalkulator Urlopu
+//
+//  Created by Tymoteusz Stokarski on 03/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

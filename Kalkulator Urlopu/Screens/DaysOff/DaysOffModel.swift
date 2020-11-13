4c14dff9745649245e185daccbb68e06f93ecbf0
Tymoteusz Stokarski <tjoelstokarski@gmail.com>
2020-11-13 03:52:43 +0100

    Corrected info text

--- Kalkulator Urlopu/Screens/DaysOff/DaysOffModel.swift
+++ Kalkulator Urlopu/Screens/DaysOff/DaysOffModel.swift
@@ -23,7 +23,7 @@ struct DaysOffResult {
 
 struct DaysOffInfo {
     
-    var message: String = "Należy pamiętać że niepełny miesiąc zatrudnienia zaokrągla się do pełnego miesiąca, np. jeżeli pracownik pracował w okresie od 15 stycznia do 16 lutego, od 15 stycznia do 14 lutego przepracował dokładnie miesiąc, natomiast od 15 lutego do 16 lutego dwa dni, które zaokrągla się do pełnego miesiąca, czyli liczba przepracowanych miesięcy wynosi 2."
-    var bulletPoints: [String] = ["urlopu bezpłatnego", "urlopu wychowawczego", "odbywania zasadniczej służby wojskowej lub jej form zastępczych, okresowej służby wojskowej, przeszkolenia wojskowego albo ćwiczeń wojskowych", "tymczasowego aresztowania", "odbywania kary pozbawienia wolności", "nieusprawiedliwionej nieobecności w pracy"]
+    var message: String = "Niepełny miesiąc zatrudnienia zaokrągla się do pełnego miesiąca - jeżeli pracownik był zatrudniony w okresie od 15 stycznia do 16 lutego, od 15 stycznia do 14 lutego przepracował dokładnie miesiąc, natomiast od 15 lutego do 16 lutego dwa dni, które zaokrąglane są do pełnego miesiąca. Liczba przepracowanychc miesięcy wynoosi 2. Z okresu zatrudniena należy wyłączyć:"
+    var bulletPoints: [String] = ["bezpłatny urlop", "urlop wychowawczy", "odbywanie zasadniczej służby wojskowej lub jej form zastępczych, okresowej służby wojskowej, przeszkolenia wojskowego albo ćwiczeń wojskowych", "tymczasowy areszt", "odbywanie kary pozbawienia wolności", "nieusprawiedliwiona nieobecność w pracy"]
     
 }

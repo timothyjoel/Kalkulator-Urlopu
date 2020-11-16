eb296665abcb3ec5b3ef496787139193a94f7ffc
Tymoteusz Stokarski <tjoelstokarski@gmail.com>
2020-11-16 02:21:37 +0100

    Fixed test for maternityviewmodel

--- Kalkulator UrlopuTests/MaternityLeaveViewModel_Tests.swift
+++ Kalkulator UrlopuTests/MaternityLeaveViewModel_Tests.swift
@@ -10,11 +10,11 @@ class MaternityLeaveViewModel_Tests: XCTestCase {
         let sut = makeSUT(kidsBorn: 1)
         let result = MaternityLeaveResult(
             maternityLeave: 20,
-            maternityLeaveStartDate: Date(day: 15, month: 11, year: 2020),
-            maternityLeaveFinishDate: Date(day: 3, month: 4, year: 2021),
+            maternityLeaveStartDate: "15.11.2020",
+            maternityLeaveFinishDate: "03.04.2021",
             parentalLeave: 32,
-            parentalLeaveStartDate: Date(day: 4, month: 4, year: 2021),
-            parentalLeaveFinishDate: Date(day: 13, month: 11, year: 2021),
+            parentalLeaveStartDate: "04.04.2021",
+            parentalLeaveFinishDate: "13.11.2021",
             summedLeave: 52)
         XCTAssertEqual(sut.result, result)
     }
@@ -23,11 +23,11 @@ class MaternityLeaveViewModel_Tests: XCTestCase {
         let sut = makeSUT(kidsBorn: 2)
         let result = MaternityLeaveResult(
             maternityLeave: 31,
-            maternityLeaveStartDate: Date(day: 15, month: 11, year: 2020),
-            maternityLeaveFinishDate: Date(day: 19, month: 6, year: 2021),
+            maternityLeaveStartDate: "15.11.2020",
+            maternityLeaveFinishDate: "19.06.2021",
             parentalLeave: 34,
-            parentalLeaveStartDate: Date(day: 20, month: 6, year: 2021),
-            parentalLeaveFinishDate: Date(day: 12, month: 2, year: 2022),
+            parentalLeaveStartDate: "20.06.2021",
+            parentalLeaveFinishDate: "12.02.2022",
             summedLeave: 65)
         XCTAssertEqual(sut.result, result)
     }
